4852fb3ea989dad101f947a590b9f9945d82129a
Minindu Madhushan <m.21madhushan@gmail.com>
2026-04-04 22:29:03 +0530

    clean user controller

--- Backend/src/main/java/com/smartlearning/backend/controller/UserController.java
+++ Backend/src/main/java/com/smartlearning/backend/controller/UserController.java
@@ -1,14 +1,5 @@
 package com.smartlearning.backend.controller;
 
-import com.smartlearning.backend.model.User;
-import com.smartlearning.backend.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-
 public class UserController {
 
     
