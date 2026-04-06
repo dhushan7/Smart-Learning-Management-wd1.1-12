51e2fd3bd3dfaa0dfbd16868dd8139f40b5820a6
Minindu Madhushan <m.21madhushan@gmail.com>
2026-04-06 12:56:54 +0530

    solve conflicted changes and update quizbank page and chatbot page align with navbar

--- Backend/src/main/java/com/smartlearning/backend/repository/TaskRepository.java
+++ Backend/src/main/java/com/smartlearning/backend/repository/TaskRepository.java
@@ -2,10 +2,11 @@ package com.smartlearning.backend.repository;
 
 import com.smartlearning.backend.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
-
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByCompletedFalse();
     List<Task> findByDueDate(LocalDate date);
